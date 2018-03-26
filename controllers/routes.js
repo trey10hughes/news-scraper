630a1630b24a3838e69478b744a1ccb120677309
Theodore hughes <theodorehughes@Theodores-MacBook-Pro.local>
2018-03-26 14:17:00 -0500

    server set up and connection to mongodb confirmed

---
+++ controllers/routes.js
@@ -0,0 +1,17 @@
+var express = require("express");
+
+var router = express.Router();
+
+var request = require("request");
+
+var cheerio = require("cheerio");
+
+var mongoose = require("mongoose");
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+mongoose.Promise = Promise;
+
+
+
+// Export routes for server.js to use.
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,55 @@
+// Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+// Scraping dependencies
+var request = require("request");
+var cheerio = require("cheerio");
+
+mongoose.Promise = Promise;
+
+var PORT = process.env.PORT || 3000;
+
+// Initialize Express
+var app = express();
+
+// Use morgan and body parser with our app
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+// Serve static content
+app.use(express.static("public"));
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/routes.js");
+
+app.use("/", routes);
+
+mongoose.connect("mongodb://heroku_f04tff6d:gmu7fb2cfvfuc3rri69nnjm5i4@ds123399.mlab.com:23399/heroku_f04tff6d");
+
+var db = mongoose.connection;
+
+// Show any mongoose errors
+db.on("error", function (error) {
+    console.log("Mongoose Error: ", error);
+});
+
+// Once logged in to the db through mongoose, log a success message
+db.once("open", function () {
+    console.log("Mongoose connection successful.");
+});
+
+// Listen on port 3000
+app.listen(PORT, function () {
+    console.log("App running on PORT " + PORT);
+});
