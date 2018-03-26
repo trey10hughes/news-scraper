79d356f81ef47d1a805465c672a75da619c024f4
Theodore hughes <theodorehughes@Theodores-MacBook-Pro.local>
2018-03-26 14:51:36 -0500

    added route to scrape articles

--- controllers/routes.js
+++ controllers/routes.js
@@ -16,6 +16,44 @@ router.get("/", function (req, res) {
 });
 
 
+// A GET request to scrape the nytimes website
+router.post("/scrape", function (req, res) {
+
+    // First, we grab the body of the html with request
+    request("http://www.nytimes.com/", function (error, response, html) {
+        // Then, we load that into cheerio and save it to $ for a shorthand selector
+        var $ = cheerio.load(html);
+
+        // Make emptry array for temporarily saving and showing scraped Articles.
+        var scrapedArticles = {};
+        // Now, we grab every h2 within an article tag, and do the following:
+        $("article h2").each(function (i, element) {
+
+            // Save an empty result object
+            var result = {};
+
+            // Add the text and href of every link, and save them as properties of the result object
+            result.title = $(this).children("a").text();
+
+            console.log("What's the result title? " + result.title);
+
+            result.link = $(this).children("a").attr("href");
+
+            scrapedArticles[i] = result;
+
+        });
+
+        console.log("Scraped Articles object built nicely: " + scrapedArticles);
+
+        var hbsArticleObject = {
+            articles: scrapedArticles
+        };
+
+        res.render("index", hbsArticleObject);
+
+    });
+});
+
 
 // Export routes for server.js to use.
 module.exports = router;
