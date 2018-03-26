1097e6e953934c75d5a55a7f0b8c628c2109edf1
Theodore hughes <theodorehughes@Theodores-MacBook-Pro.local>
2018-03-26 14:44:01 -0500

    added article model

--- controllers/routes.js
+++ controllers/routes.js
@@ -11,6 +11,10 @@ var mongoose = require("mongoose");
 // Set mongoose to leverage built in JavaScript ES6 Promises
 mongoose.Promise = Promise;
 
+router.get("/", function (req, res) {
+    res.render("index");
+});
+
 
 
 // Export routes for server.js to use.
